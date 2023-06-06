a8cdc4ea9d5fe6d6435c33835f4d6115c86ffb60
Marcello92tp <marcellokgnoctis92@gmail.com>
2023-06-06 16:27:19 +0200

    esercio ingresso e uscita 2 svolto

---
+++ printyournameandsurname.java
@@ -0,0 +1,7 @@
+public class printyournameandsurname {
+    public static void main(String[] args){
+        String name = "Marcello";
+        String surname = "Gerardi";
+        System.out.println(name+" "+surname);
+    }
+}
